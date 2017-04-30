e3d2223e92ede6ee45525bf210cd33506747e49e
buyulian <67764256@qq.com>
2017-04-30 16:29:06 +0800

    翻转单链表，时间复杂度o(n)，空间复杂度o(1)

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -1,13 +1,13 @@
 //翻转单链表，时间复杂度o(n)，空间复杂度o(1)
-public class ReverseList{
-    class list{
-        int val;
-        list next;
-        list(int n){
-            val=n;
-            next=null;
-        }
+class list{
+    int val;
+    list next;
+    list(int n){
+        val=n;
+        next=null;
     }
+}
+public class ReverseList{
     public static list reverseList(list head){
         if(head==null)return head;
         list p,q;

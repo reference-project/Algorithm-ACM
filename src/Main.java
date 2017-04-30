22ede2833c819075d29bda8c07d9ee4b0d3b200f
buyulian <67764256@qq.com>
2017-04-30 16:48:27 +0800

    Polya定理，c种颜色的珠子，组成长为n的项链，项链没有方向和起始位置。

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        list a=new list(0);
-        list b=new list(1);
-        list c=new list(2);
-        list d=new list(3);
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        d.next=null;
-        a=ReverseList.reverseList(a);
-        for(list e=a;e!=null;e=e.next)
-            System.out.println(e.val);
+        System.out.println(Polya.polya(3,4));
     }
 }
--- src/Polya.java
+++ src/Polya.java
@@ -1,6 +1,10 @@
-//Polya定理，c种颜色的珠子，组成长为s的项链，项链没有方向和起始位置。
+//Polya定理，c种颜色的珠子，组成长为n的项链，项链没有方向和起始位置。
 public class Polya {
-    public static int polya(int c,int a){
-
+    public static int polya(int c,int n){
+        int s=0;
+        s+=Math.pow(c,n);
+        for(int i=1;i<n;i++)s+=Math.pow(c,Gcd.gcd(n,i));
+        s/=n;
+        return s;
     }
 }
